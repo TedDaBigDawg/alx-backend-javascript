2c59bf8f130d1d12f8d0431e0ea4d0a4bfeb66b6
Ted Osiobe <osiobeted@gmail.com>
2023-06-27 11:06:04 +0000

    Final Commit

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,7 +1,7 @@
 export default function getResponseFromAPI() {
   return new Promise((resolve, reject) => {
     if (1 + 1 == 2) reject(new Error('Error'));
-    resolve(console.log("Success"););
+    resolve('Success');
   });
 }
 
